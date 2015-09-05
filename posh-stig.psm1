5aceeebdf997690c59f28eb3724871e042fa8d9b
thezim <thezim@gmail.com>
2015-09-05 15:29:41 -0400

    Use nested objects.
    Now returns nested objects instead of a flattened object. Now inline
    with the structure of the XML of the checklist. All properties have been
    pascal cased .

--- posh-stig.psm1
+++ posh-stig.psm1
@@ -4,19 +4,18 @@
 		[ValidateScript({Test-Path -PathType Leaf -Path $_})]
         [string]$Path
     )
-    $data = @()
     $checklist = ([Xml](Get-Content -Raw -Path $Path)).CHECKLIST
-    $info = @{        
-        SV_VERSION = $checklist.SV_VERSION
-        STIG_TITLE = $checklist.STIG_INFO.STIG_TITLE
-    }
+    $assetinfo = @{}
+    # parse asset information
     foreach($node in $checklist.ASSET.ChildNodes){
         if($node.Name -ne "ASSET_VAL"){
-            $info["$($node.Name)"] = $node.'#text'
+            $assetinfo["$($node.Name)"] = $node.'#text'
         } else {
-            $info["$($node.AV_NAME.'#text')"] = $node.AV_DATA.'#text'
+            $assetinfo["$($node.AV_NAME.'#text')"] = $node.AV_DATA.'#text'
         }
     }
+    $vulns = @()
+    # parse vulnerablility data
     foreach($vuln in $checklist.VULN){
         $vulninfo = @{}
         foreach($node in $vuln.ChildNodes){
@@ -26,9 +25,53 @@
                 $vulninfo["$($node.Name)"] = $node.'#text'
             }
         }
-        $data += New-Object PSObject -Property ($vulninfo += $info)
+        $vulns += New-Object PSObject -Property @{
+            Comments = $vulninfo.COMMENTS
+            FindingDetails = $vulninfo.FINDING_DETAILS
+            SeverityJustification = $vulninfo.SEVERITY_JUSTIFICATION
+            SeverityOverride = $vulninfo.SEVERITY_OVERRIDE
+            Status = $vulninfo.STATUS                   
+            Data = New-Object PSObject -Property @{
+                CheckContent = $vulninfo.Check_Content
+                Check_Content_Ref = $vulninfo.Check_Content_Ref
+                Class = $vulninfo.Class
+                Documentable = $vulninfo.Documentable
+                FalseNegatives = $vulninfo.False_Negatives
+                FalsePositives = $vulninfo.False_Positives
+                FixText = $vulninfo.Fix_Text
+                GroupTitle = $vulninfo.Group_Title
+                IAControls = $vulninfo.IA_Controls
+                MitigationControl = $vulninfo.Mitigation_Control
+                Mitigations = $vulninfo.Mitigations
+                Potential_Impact = $vulninfo.Potential_Impact
+                Responsibility = $vulninfo.Responsibility
+                RuleId = $vulninfo.Rule_ID
+                RuleTitle = $vulninfo.Rule_Title
+                RuleVersion = $vulninfo.Rule_Ver
+                STIGRef = $vulninfo.STIGRef
+                SecurityOverrideGuidance = $vulninfo.Security_Override_Guidance
+                Severity = $vulninfo.Severity
+                TargetKey = $vulninfo.TargetKey
+                ThirdPartyTools = $vulninfo.Third_Party_Tools
+                VulnDiscuss = $vulninfo.Vuln_Discuss
+                VulnNumber = $vulninfo.Vuln_Num
+           }
+        }
     }
-    $data
+    # return object
+    New-Object PSObject -Property @{
+        Title = $checklist.STIG_INFO.STIG_TITLE
+        Version = $checklist.SV_VERSION
+        Vulnerabilities = $vulns
+        Asset = New-Object PSObject -Property @{
+            AssetType = $assetinfo.ASSET_TYPE
+            HostIP = $assetinfo.HOST_IP
+            HostMAC = $assetinfo.HOST_MAC
+            HostName = $assetinfo.HOST_NAME
+            Role = $assetinfo.ROLE
+            TargetKey = $assetinfo.TARGET_KEY
+        }
+    }
 }
 
 Set-Alias -Name 'gckl' -Value 'Get-Checklist' -Confirm:$false

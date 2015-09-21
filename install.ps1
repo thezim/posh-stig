85a0267976f19425591fec11f2c1ed65c621f41e
thezim <thezim@gmail.com>
2015-09-21 09:51:28 -0400

    Add installation script.

---
+++ install.ps1
@@ -0,0 +1,6 @@
+﻿$files = @("posh-stig.psd1", "posh-stig.psm1")
+$modpath = "{0}\posh-stig" -f ($env:PSModulePath -split ';' | Select-Object -First 1)
+if((Test-Path -Path $modpath) -eq $false){
+    New-Item -ItemType Directory -Path $modpath | Out-Null
+}
+Copy-Item -Force -Path $files -Destination $modpath

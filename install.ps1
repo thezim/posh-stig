65cae4d46c5b56d7e5fd66aa5e3cacdaeeed5cd7
Greg Zimmerman <thezim@gmail.com>
2017-01-25 14:04:20 -0500

    Remove BOM.

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-﻿$files = @("posh-stig.psd1", "posh-stig.psm1")
+$files = @("posh-stig.psd1", "posh-stig.psm1")
 $modpath = "{0}\posh-stig" -f ($env:PSModulePath -split ';' | Select-Object -First 1)
 if((Test-Path -Path $modpath) -eq $false){
     New-Item -ItemType Directory -Path $modpath | Out-Null

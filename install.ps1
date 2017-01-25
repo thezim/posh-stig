1a9a62cd0e6bafa15e4bd3866d656977fa02945c
Greg Zimmerman <thezim@gmail.com>
2017-01-25 14:17:57 -0500

    Make installer OS agnostic.

--- install.ps1
+++ install.ps1
@@ -1,5 +1,6 @@
 $files = @("posh-stig.psd1", "posh-stig.psm1", "README.md")
-$modpath = "{0}\posh-stig" -f ($env:PSModulePath -split ';' | Select-Object -First 1)
+$delimiter = [System.IO.Path]::PathSeparator
+$modpath = "{0}\posh-stig" -f ($env:PSModulePath -split $delimiter | Select-Object -First 1)
 if((Test-Path -Path $modpath) -eq $false){
     New-Item -ItemType Directory -Path $modpath | Out-Null
 }
